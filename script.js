f851ff18a2b724ac949f7240e6428fcf1ce30a0b
Shivam Shewa <shewashivam@gmail.com>
2020-10-26 13:28:44 -0400

    Updated HTML with Divs and added to searchForWeather function

--- script.js
+++ script.js
@@ -5,16 +5,26 @@ $(document).ready(function () {
 
 
     function searchForWeather(inputReceived) {
+        $('.weatherDashboard').empty();
         console.log(inputReceived)
-
-        let queryURL = "http://api.openweathermap.org/data/2.5/weather?appid=" + apiKey + "&units=imperial&q=" + inputReceived;
+        let temperature = $('<div class = "temperature">');
+        let humidity = $('<div class = "humidity">');
+        let wind = $('<div class = "wind">');
+        let queryURL = `http://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=imperial&q=${inputReceived}`;
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
             console.log(response);
+            temperature.text(`Temperature: ${response.main.temp} °F`);
+            humidity.text(`Humidity: ${response.main.humidity}%`);
+            wind.text(`Wind Speed: ${response.main.humidity} MPH`);
+
+
+            $(".weatherDashboard").append(temperature).append(humidity).append(wind);
         });
     }
+    
 
     userClick.on("click", function () {
         if (userSearch.val()) {
